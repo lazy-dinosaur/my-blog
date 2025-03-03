dddc899115a1bc7b9f928f8191fcd7af5b7e8197
lazydino <woohs0130@naver.com>
2025-03-03 14:30:35 +0900

    Fix: 포스팅 에러 해결중

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
     domains: ["user-images.githubusercontent.com"],
   },
   /* config options here */
+  // 프로덕션 빌드에 정적 파일 포함
+  output: "standalone",
+  // 서버 미들웨어 조정
+  experimental: {
+    serverComponentsExternalPackages: ["gray-matter"],
+  },
 };
 
 export default nextConfig;
