0e9a7f1c9fe2d09dc5d6d65e64e0667b910f663c
lazydino <woohs0130@naver.com>
2025-03-03 11:31:54 +0900

    Chore: 테스트 설정 완료

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     domains: ["user-images.githubusercontent.com"],
     unoptimized: true,
   },
+  trailingSlash: true, // ✅ 정적 서버 라우팅 호환성
   /* config options here */
 };
 
